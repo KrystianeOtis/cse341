968a892bf7ecafe720485ccaa693c1a9156ba040
Krystiane Otis <oti19001@byui.edu>
2023-01-21 23:02:27 -0700

    lesson3

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,8 +3,15 @@ const router = express.Router();
 
 const contactsController = require('../controllers/contacts');
 
+
 router.get('/', contactsController.getAll);
 
 router.get('/:id', contactsController.getSingle);
+
+router.post('/edit/content', contactsController.getAddContact);
+
+router.put('/:id', contactsController.updateContact);
+
+router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
